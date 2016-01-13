7b08a27d64b9bc09128d045416a6046e25f9acff
garyliangge <garyliangge@berkeley.edu>
2016-01-13 03:15:02 -0500

    Logo size adjustments

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,73 +1,82 @@
 $(document).ready(function()
 {
-    setInitialSize();
-    window.onresize = checkResize;
 
-    $("#navbar > ul > li > a").click(function(e) {
-        // Prevent a page reload when a link is pressed
-        e.preventDefault();
-        // Call the scroll function
-        goToByScroll(this.id);
-    });
+  window.onresize = checkResize;
+  $("#navbar > ul > li > a").click(function(e) {
+    // Prevent a page reload when a link is pressed
+    e.preventDefault();
+    // Call the scroll function
+    goToByScroll(this.id);
+  });
 
-    $("#chevron > a").click(function(e) {
-        // Prevent a page reload when a link is pressed
-        e.preventDefault();
-        // Call the scroll function
-        goToByScroll(this.id);
-    });
+  $("#chevron > a").click(function(e) {
+    // Prevent a page reload when a link is pressed
+    e.preventDefault();
+    // Call the scroll function
+    goToByScroll(this.id);
+  });
 
-    $(window).scroll(onScroll);
+  $(window).scroll(onScroll);
+
+  setInitialSize();
+
 });
 
-function setInitialSize() {
-    alert("Ratio: " + (window.innerWidth/window.innerHeight) + " Width: " + window.innerWidth);
-}
 
 function goToByScroll(id){
-    // Remove "link" from the ID
-    id = id.replace("link", "");
-    // Scroll
-    $('html,body').animate({
-        scrollTop: $("#"+id).offset().top},
-        'medium');
+  // Remove "link" from the ID
+  id = id.replace("link", "");
+  // Scroll
+  $('html,body').animate({
+    scrollTop: $("#"+id).offset().top},
+    'medium');
 }
 
 function onScroll() {
-    // Change color of navbar
-    if($(this).scrollTop() == 0) {
-        $('#nav').removeClass('opaque');
-        $('#nav').addClass('transparent');
-    } else {
-        $('#nav').removeClass('transparent');
-        $('#nav').addClass('opaque');
-    }
+  // Change color of navbar
+  if($(this).scrollTop() == 0) {
+    $('#nav').removeClass('opaque');
+    $('#nav').addClass('transparent');
+  } else {
+    $('#nav').removeClass('transparent');
+    $('#nav').addClass('opaque');
+  }
 
-    //Check active section
-    var scrollPos = Math.floor($(document).scrollTop());
-    $("#navbar a").each(function() {
-        var refId = $(this).attr("id");
-        var refSecId = $(this).attr("id");
-        refId = "#".concat(refId.replace("link", ""));
-        refSecId = "#".concat(refSecId.replace("link", "_body"))
-        var refElement = $(refId);
-        var refSecElement = $(refSecId);
-        if (Math.floor(refElement.position().top) <= scrollPos &&
-            Math.floor(refElement.position().top + refElement.height() + refSecElement.height()) > scrollPos) {
-            $("#navbar > ul > li > a").removeClass("active");
-            $(this).addClass("active");
-        } else {
-            $(this).removeClass("active");
-        }
-    });
+  //Check active section
+  var scrollPos = Math.floor($(document).scrollTop());
+  $("#navbar a").each(function() {
+    var refId = $(this).attr("id");
+    var refSecId = $(this).attr("id");
+    refId = "#".concat(refId.replace("link", ""));
+    refSecId = "#".concat(refSecId.replace("link", "_body"))
+    var refElement = $(refId);
+    var refSecElement = $(refSecId);
+    if (Math.floor(refElement.position().top) <= scrollPos &&
+      Math.floor(refElement.position().top + refElement.height() + refSecElement.height()) > scrollPos) {
+      $("#navbar > ul > li > a").removeClass("active");
+      $(this).addClass("active");
+    } else {
+      $(this).removeClass("active");
+    }
+  });
+}
+
+function setInitialSize() {
+  alert("Ratio: " + (window.innerWidth/window.innerHeight) + " Width: " + window.innerWidth);
+  // checkResize();
 }
 
 function checkResize() {
-    width, height = window.innerWidth, window.innerHeight;
-    if (width/height < 1.5) {
-        document.getElementById("sbu_logo").style.height = min(300, width*.6)+"px";
-    }else{
-        document.getElementById("sbu_logo").style.height = min(300, width*.4)+"px";
-    }
-    alert(window.innerWidth/window.innerHeight);
+  width = window.innerWidth;
+  height = window.innerHeight;
+  // alert("Ratio: " + (width/height) + " Width: " + width);
+  if ((width/height) < 1.5) {
+    document.getElementById("flowchart").style.display = "none";
+    document.getElementById("sbu_logo").style.width = Math.min(300, width*.6)+"px";
+
+  }else{
+    document.getElementById("flowchart").style.display = "table";
+    document.getElementById("sbu_logo").style.width = Math.min(300, width*.3)+"px";
+
+  }
 }
